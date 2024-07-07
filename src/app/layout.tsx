115e5783b3cd7067121761872a2a3867620cb335
noobiecoder1942 <abhiragnagpure@gmail.com>
2024-07-07 17:57:00 -0400

    Added Theme Switcher for dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
 import { useState } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,15 +24,25 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} flex items-start justify-between`}>
         
-      <div className={`transition-all min-h-screen border-r 
-        ${isSidebarOpen ? 'w-[300px] min-w-[300px]' : 'w-[50px] min-w-[50px]'}`}>
-            <Sidebar onToggle={handleToggleSidebar} />
-          </div>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className={`transition-all min-h-screen border-r 
+              ${isSidebarOpen ? 'w-[300px] min-w-[300px]' : 'w-[50px] min-w-[50px]'}`}>
+                <Sidebar onToggle={handleToggleSidebar} />
+            </div>
 
-          <main className="grid w-full h-full">
-            <Header />
-            {children}
-          </main>
+            <main className="grid w-full h-full">
+              <Header />
+              {children}
+            </main>
+        </ThemeProvider>
+
+
+
         </body>
     </html>
   );
